5d900a92b73fe026f31f51f687750dfd997f302c
José Renato Royer <jrenato_royer@outlook.com>
2022-06-07 18:51:55 -0300

    initialize

---
+++ project/src/test/java/consultar/cep/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package consultar.cep.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
